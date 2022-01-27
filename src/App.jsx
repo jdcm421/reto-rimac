bb4649a784eb270eaaa936a263bb2d1a12597bc1
Juan Diego Caceres Munares <44043720+juandiego436@users.noreply.github.com>
2022-01-27 14:59:30 -0500

    cambiar modelo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import Routes from "../src/routes/Routes";
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { SnackBarContext } from "../src/context/snackbar";
-import { Snackbar, SnackbarContent } from '@material-ui/core';
+import HomeComponent from "./components/views/HomeComponent";
+
 
 const App = () => {
-    const [snackBar, setSnackBar] = useState({ open: false, variant: 'default', text: '' });
 
     return (
         <div id="app">
-            <SnackBarContext.Provider>
-              <Router>
-                <Routes />
-              </Router>
-            </SnackBarContext.Provider>
+          <HomeComponent />
         </div>
     )
 };
--- src/context/snackbar.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const SnackBarContext = createContext(true);
-
-export function useSnackBar() {
-  return useContext(SnackBarContext);
-}
